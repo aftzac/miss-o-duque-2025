501a3bc45884dd9f42723f7c9813fa26ae2d47ec
aftzac <162328884+aftzac@users.noreply.github.com>
2025-06-26 11:10:08 -0300

    pinto

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincpal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".texto-resultado");
