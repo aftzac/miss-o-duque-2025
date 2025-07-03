f97755df6dbd9c4ec40e399b34d4acfbf07d295e
aftzac <162328884+aftzac@users.noreply.github.com>
2025-07-03 11:16:07 -0300

    hlvkyhyhilgvblui

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultado = document.querySelector(".texto-resultado");
+
+const perguntas=[
+    {
+        enunciado:"Qual foi o ultimo brasileiro a ganhar a bola de ouro?",
+        alternativas:[
+            {
+                texto:"Ronaldo Fenômeno",
+                afirmação:"Resposta Errada",
+                pontos:0
+            },
+            {
+                texto:"Kaká",
+                afirmação:"Resposta Correta",
+                pontos:1
+            }
+        ]
+    }
+    {
+        enunciado:"Qual foi o ultimo brasileiro a ganhar a bola de ouro?",
+        alternativas:[
+            {
+                texto:"Ronaldo Fenômeno",
+                afirmação:"Resposta Errada",
+                pontos:0
+            },
+            {
+                texto:"Kaká",
+                afirmação:"Resposta Correta",
+                pontos:1
+            }
+        ]
+    }
+]
